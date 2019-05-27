dcb6765703a0022d6dd6da869603f9598c5776b7
lucien <thewebress@gmail.com>
2019-05-27 14:20:28 +0800

    format code

--- slimjwt.php
+++ slimjwt.php
@@ -2,11 +2,11 @@
 
 class Slimjwt
 {
-	//encode
-	public static function encode($payload,$key,$alg = 'HS256',$keyId = null,$head = null)
+    //encode
+    public static function encode($payload,$key,$alg = 'HS256',$keyId = null,$head = null)
     {
         //$key = self::urlsafeB64Encode($key);
-		$header = array('typ' => 'JWT', 'alg' => $alg);
+        $header = array('typ' => 'JWT', 'alg' => $alg);
         if ($keyId !== null) {
             $header['kid'] = $keyId;
         }
@@ -17,55 +17,55 @@ class Slimjwt
         return $jwt . '.' . self::signature($jwt, $key, $alg);
     }
 
-	//sign
-	public static function signature($input,$key,$method = 'HS256')
-	{
-		$methods = array(
-            'HS256' => 'sha256',
-            'HS384' => 'sha384',
-            'HS512' => 'sha512',
-        );
-		if (empty($methods[$method])) {
-            throw new DomainException('Algorithm not supported');
-        }
-		return self::urlsafeB64Encode(hash_hmac($methods[$method], $input, $key,true));
-	}
+    //sign
+    public static function signature($input,$key,$method = 'HS256')
+    {
+        $methods = array(
+        'HS256' => 'sha256',
+        'HS384' => 'sha384',
+        'HS512' => 'sha512',
+    );
+        if (empty($methods[$method])) {
+        throw new DomainException('Algorithm not supported');
+    }
+        return self::urlsafeB64Encode(hash_hmac($methods[$method], $input, $key,true));
+    }
 
-	//decode
-	public static function decode($jwt,$key)
-	{
-		$tokens = explode('.', $jwt);
-		//$key    = self::urlsafeB64Encode($key);
-		if (count($tokens)!=3)
-			return false;
+    //decode
+    public static function decode($jwt,$key)
+    {
+        $tokens = explode('.', $jwt);
+        //$key    = self::urlsafeB64Encode($key);
+        if (count($tokens)!=3)
+            return false;
 
-		list($header64, $payload64, $sign) = $tokens;
+        list($header64, $payload64, $sign) = $tokens;
 
-		$header = json_decode(self::urlsafeB64Decode($header64), true);
-		if (empty($header['alg']))
-			return false;
+        $header = json_decode(self::urlsafeB64Decode($header64), true);
+        if (empty($header['alg']))
+            return false;
 
-		if (self::signature($header64 . '.' . $payload64, $key, $header['alg']) !== $sign)
-			return false;
-		$payload = json_decode(self::urlsafeB64Decode($payload64), true);
+        if (self::signature($header64 . '.' . $payload64, $key, $header['alg']) !== $sign)
+            return false;
+        $payload = json_decode(self::urlsafeB64Decode($payload64), true);
 
-		$time = $_SERVER['REQUEST_TIME'];
-		if (isset($payload['iat']) && $payload['iat'] > $time)
-			return false;
-		if (isset($payload['nbf']) && $payload['nbf'] > $time)
-			return false;
-		if (isset($payload['exp']) && $payload['exp'] < $time)
-			return false;
-		return $payload;
-	}
-	
-	//urlsafeB64Encode
+        $time = $_SERVER['REQUEST_TIME'];
+        if (isset($payload['iat']) && $payload['iat'] > $time)
+            return false;
+        if (isset($payload['nbf']) && $payload['nbf'] > $time)
+            return false;
+        if (isset($payload['exp']) && $payload['exp'] < $time)
+            return false;
+        return $payload;
+    }
+    
+    //urlsafeB64Encode
     public static function urlsafeB64Encode($input)
     {
         return str_replace('=', '', strtr(base64_encode($input), '+/', '-_'));
     }
 
-	//urlsafeB64Decode
+    //urlsafeB64Decode
     public function urlsafeB64Decode($input)
     {
         $remainder = strlen($input) % 4;
